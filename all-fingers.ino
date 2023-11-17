2ca8b12cf53a6edeb5a2dda5cb489a92418d34ff
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2023-11-17 11:57:29 -0500

    Update and rename test.ino to all-fingers.ino

--- all-fingers.ino
+++ all-fingers.ino
@@ -19,14 +19,13 @@ String receivedString;
 void setup() {
   // put your setup code here, to run once:
   Serial. begin(9600);
-  servoThumb.attach(7);
-  servoIndex.attach(7);
-  servoMiddle.attach(7);
-  servoRing.attach(7);
-  servoPinky.attach(7);
-
-
+  servoThumb.attach(2);
+  servoIndex.attach(3);
+  servoMiddle.attach(4);
+  servoRing.attach(5);
+  servoPinky.attach(6);
 }
+
 void receiveData(){
   while (Serial.available()){
     char c = Serial.read();
@@ -49,8 +48,13 @@ void receiveData(){
     }
   }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
+void loop(){
+  receiveData();
+  if (valsRec[0]==1){servoThumb.write(180);}else{servoThumb.write(0);}
+  if (valsRec[1]==1){servoIndex.write(180);}else{servoIndex.write(0);}
+  if (valsRec[2]==1){servoMiddle.write(180);}else{servoMiddle.write(0);}
+  if (valsRec[3]==1){servoRing.write(180);}else{servoRing.write(0);}
+  if (valsRec[4]==1){servoPinky.write(180);}else{servoPinky.write(0);}
+}
+  
